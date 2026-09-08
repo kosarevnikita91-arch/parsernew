300283fe805ed47e1f9e810ee2db9ae755eed0ea
kosarevnikita91-arch <kosarevnikita91@gmail.com>
2026-09-08 18:42:54 +0500

    Update bott.py

--- bott.py
+++ bott.py
@@ -41,14 +41,7 @@ load_dotenv()
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 
-BASE_DIR = Path(__file__).resolve().parent
-SESSION_PATH = BASE_DIR / "telegram_profile"
 
-telegram_client = TelegramClient(
-     str(SESSION_PATH),
-    api_id,
-    api_hash
-)
 
 
 
