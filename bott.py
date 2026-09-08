97f0069bb706942ea66cc6bdf0b1fa45020f1939
kosarevnikita91-arch <kosarevnikita91@gmail.com>
2026-09-08 17:32:49 +0500

    Update bott.py

--- bott.py
+++ bott.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from dotenv import load_dotenv
 
 from telethon import TelegramClient
 from telethon.errors import FloodWaitError
