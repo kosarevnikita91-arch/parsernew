a43105a90f357c62edd024d36f3ca23ff94e9f69
kosarevnikita91-arch <kosarevnikita91@gmail.com>
2026-09-08 17:11:39 +0500

    Update bott.py

--- bott.py
+++ bott.py
@@ -25,14 +25,14 @@ from telethon.tl.functions.channels import (
 from telethon.tl.functions.messages import (
     GetDialogFiltersRequest,
 )
-from telethon.tl.types import (
-    ChannelParticipantAdmin,
-    ChannelParticipantCreator,
-    DialogFilter,
-    UserStatusOnline,
-    UserStatusRecently,
-    UserStatusLastWeek,
-    UserStatusLastMonth,
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
+from telethon.tl.types import DialogFilter
+from telethon.tl.types import UserStatusOnline
+from telethon.tl.types import UserStatusRecently
+from telethon.tl.types import UserStatusLastWeek
+from telethon.tl.types import UserStatusLastMonth
+
 )
 telegram_client = TelegramClient(
     "/app/telegram_profile",
